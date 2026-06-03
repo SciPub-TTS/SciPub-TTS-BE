4617bdac6234d61ab996d384d1b8bfe250a2fe5c
Võ Đức Trí <voductri88714@gmail.com>
2026-06-03 21:44:24 +0700

    fix(security): add resolver to check princical

--- scipubtts/src/main/java/com/brotherhood/scipubtts/auth/security/UserPrincipal.java
+++ scipubtts/src/main/java/com/brotherhood/scipubtts/auth/security/UserPrincipal.java
@@ -31,7 +31,6 @@ public class UserPrincipal implements UserDetails, OAuth2User, CredentialsContai
     }
 
     public static UserPrincipal create(User user) {
-        System.out.println(user.getId());
         return new UserPrincipal(
                 user.getId(),
                 user.getEmail(),
--- scipubtts/src/main/java/com/brotherhood/scipubtts/user/controller/AccountController.java
+++ scipubtts/src/main/java/com/brotherhood/scipubtts/user/controller/AccountController.java
@@ -1,6 +1,7 @@
 package com.brotherhood.scipubtts.user.controller;
 
 import com.brotherhood.scipubtts.auth.security.UserPrincipal;
+import com.brotherhood.scipubtts.common.annotation.CurrentUserUUID;
 import com.brotherhood.scipubtts.common.apiResponse.ResponseObject;
 import com.brotherhood.scipubtts.user.dto.request.ChangePasswordRequest;
 import com.brotherhood.scipubtts.user.service.AccountService;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/api/account")
 @RequiredArgsConstructor
@@ -21,10 +24,11 @@ public class AccountController {
     private final AccountService accountService;
 
     @PostMapping("/change-password")
-    public ResponseEntity<ResponseObject> changePassword(@AuthenticationPrincipal UserPrincipal principal,
-                                                         @RequestBody ChangePasswordRequest request) {
+    public ResponseEntity<ResponseObject> changePassword(
+            @CurrentUserUUID UUID userId,
+            @RequestBody ChangePasswordRequest request) {
 
-        accountService.changePassword(principal.getId(), request);
+        accountService.changePassword(userId, request);
 
         return ResponseEntity.ok(
                 new ResponseObject(
