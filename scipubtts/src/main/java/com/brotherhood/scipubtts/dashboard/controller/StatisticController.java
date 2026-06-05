5fb2169f305384ed5051933aca558909e3c0b4d6
Huy Phong <touman876@gmail.com>
2026-06-05 09:21:37 +0700

    feat(dashboard): add new service and update controller for metrics

--- scipubtts/src/main/java/com/brotherhood/scipubtts/dashboard/controller/StatisticController.java
+++ scipubtts/src/main/java/com/brotherhood/scipubtts/dashboard/controller/StatisticController.java
@@ -1,7 +1,9 @@
 package com.brotherhood.scipubtts.dashboard.controller;
 
 import com.brotherhood.scipubtts.common.apiResponse.ResponseObject;
+import com.brotherhood.scipubtts.dashboard.dto.request.MetricRequest;
 import com.brotherhood.scipubtts.dashboard.dto.request.OpenAlexPublicationRequest;
+import com.brotherhood.scipubtts.dashboard.service.MetricService;
 import com.brotherhood.scipubtts.dashboard.service.PublicationService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
@@ -14,16 +16,31 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/statistic")
-public class PublicationController {
+public class StatisticController {
   private final PublicationService publicationService;
+  private final MetricService metricService;
 
-  public PublicationController(PublicationService publicationService) {
+  public StatisticController(PublicationService publicationService, MetricService metricService) {
     this.publicationService = publicationService;
+    this.metricService = metricService;
   }
 
   @PostMapping("/publication-trends")
   public ResponseEntity<ResponseObject> takePublicationTrend(@Valid @RequestBody OpenAlexPublicationRequest request, HttpServletRequest httpServletRequest){
     var data = publicationService.takePublicationTrends(request);
+
+    return ResponseEntity.ok(
+            new ResponseObject(
+                    HttpStatus.OK.value(),
+                    "Get publication trends successfully",
+                    data
+            )
+    );
+  }
+
+  @PostMapping("/metrics")
+  public ResponseEntity<ResponseObject> takeMetrics(@Valid @RequestBody MetricRequest request, HttpServletRequest httpServletRequest){
+    var data = metricService.takeMetrics(request);
 
     return ResponseEntity.ok(
             new ResponseObject(
--- scipubtts/src/main/java/com/brotherhood/scipubtts/dashboard/service/OpenAlexService.java
+++ scipubtts/src/main/java/com/brotherhood/scipubtts/dashboard/service/OpenAlexService.java
@@ -1,6 +1,10 @@
 package com.brotherhood.scipubtts.dashboard.service;
 
+import com.brotherhood.scipubtts.dashboard.constant.OpenAlexEntity;
+import com.brotherhood.scipubtts.dashboard.dto.request.OpenAlexMetricsInPeriodRequest;
+import com.brotherhood.scipubtts.dashboard.dto.request.OpenAlexMetricsToPeriodRequest;
 import com.brotherhood.scipubtts.dashboard.dto.request.OpenAlexPublicationRequest;
+import com.brotherhood.scipubtts.dashboard.dto.response.OpenAlexMetricsResponse;
 import com.brotherhood.scipubtts.dashboard.dto.response.OpenAlexPublicationResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
@@ -35,5 +39,59 @@ public class OpenAlexService {
                             .build())
             .retrieve()
             .body(OpenAlexPublicationResponse.class);
+  }
+
+  public OpenAlexMetricsResponse takeMetricsInPeriod(OpenAlexMetricsInPeriodRequest request){
+    if(request.endTime().isEmpty() || request.startTime().isEmpty()){
+      return null;
+    }
+
+    var queryPeriod = String.format("from_publication_date:%s,to_publication_date:%s",
+            request.startTime(), request.endTime());
+
+    return restClient.get()
+            .uri(uriBuilder ->
+                    uriBuilder
+                            .path("/works")
+                            .queryParam(
+                                    "filter",
+                                    queryPeriod
+                            )
+                            .queryParam(
+                                    "page",
+                                    1
+                            ).queryParam(
+                                    "per_page",
+                                    1
+                            ).queryParam(
+                                    "select",
+                                  "id"
+                            )
+                            .build())
+            .retrieve()
+            .body(OpenAlexMetricsResponse.class);
+  }
+
+  public OpenAlexMetricsResponse takeMetricsToPeriod(
+          OpenAlexMetricsToPeriodRequest request) {
+    var path = String.format(("/%s"), request.entity().getPath());
+
+    return restClient.get()
+            .uri(uriBuilder ->
+                    uriBuilder
+                            .path(path)
+                            .queryParam(
+                                    "page",
+                                    1
+                            ).queryParam(
+                                    "per_page",
+                                    1
+                            ).queryParam(
+                                    "select",
+                                    "id"
+                            )
+                            .build())
+            .retrieve()
+            .body(OpenAlexMetricsResponse.class);
   }
 }
