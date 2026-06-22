49a70e3e2ace581d5dd562814af81de9da7c0a1e
Võ Đức Trí <voductri88714@gmail.com>
2026-06-22 08:17:11 +0700

    fix:delete duplicate user id logic on account service

--- scipubtts/src/main/java/com/brotherhood/scipubtts/user/service/impl/AccountServiceImpl.java
+++ scipubtts/src/main/java/com/brotherhood/scipubtts/user/service/impl/AccountServiceImpl.java
@@ -90,7 +90,6 @@ public class AccountServiceImpl implements AccountService {
         User finalUser = user;
         UserProfile profile = userProfileRepository.findById(userId)
                 .orElseGet(() -> UserProfile.builder()
-                        .userId(userId)
                         .user(finalUser)
                         .build());
 
