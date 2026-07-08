8bc7c2e6f09217f18e6a9a49e423986e6333598c
Huy Phong <touman876@gmail.com>
2026-07-08 11:18:33 +0700

    feat: pull from prod

--- scipubtts/src/main/java/com/brotherhood/scipubtts/email/service/impl/EmailServiceImpl.java
+++ scipubtts/src/main/java/com/brotherhood/scipubtts/email/service/impl/EmailServiceImpl.java
@@ -11,7 +11,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-import org.thymeleaf.context.Context;
+import org.thymeleaf.contextg.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import java.nio.charset.StandardCharsets;
